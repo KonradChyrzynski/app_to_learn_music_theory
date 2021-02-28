8420cad71ef1ef7383ce9cf8e1b9856d91d27746
Konrad Chyrzynski <konrad.chyrzynski@gmail.com>
2021-02-28 19:53:39 +0100

    comit

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+
+random1 = random.randint(0,11)
+random2 = random.randint(0,5)
+
+progi = random.randint(0,12)
+
+sring_E = ["E","F","Fis","G","A","Ais","H","C","Cis","D","Dis"]
+
+
+
+strings = {"6": sring_E ,"5": (),"4": (),"3": (),"2":(),"1":()}
+
+
+
+
+sounds = ["C", "CIS", "d", "dis", "e", "f", "fis", "g", "gis", "a", "ais", "h"]
+
+
+
+up_sound=sounds[random1].upper()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
