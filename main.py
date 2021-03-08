9d470328cebb68bea294fb400c3868d44d9b88d5
KonradChyrzynski <77024537+KonradChyrzynski@users.noreply.github.com>
2021-03-08 19:52:17 +0100

    Add files via upload
    Update 2.0.0

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ def main():
 
     random2 = random.randint(1,6)
 
-    progi = random.randint(0, 11)
+    progi = random.randint(0,11)
 
 
-    e_o_e, f_o_e, fis_o_e, g_o_e, gis_o_e, a_o_e, ais_o_e, h_o_e, c_o_e, cis_o_e, d_o_e, dis_o_e = ["E", "F", "FIS", "G","GIS", "A", "AIS", "H", "C", "CIS", "D", "DIS"]
-    e_o_e1, f_o_e1, fis_o_e1, g_o_e1, gis_o_e1, a_o_e1, ais_o_e1, h_o_e1, c_o_e1, cis_o_e1, d_o_e1, dis_o_e1 = ["E", "F","FIS", "G","GIS", "A","AIS", "H","C", "CIS","D", "DIS"]
-    a_o_a, ais_o_a, h_o_a, c_o_a, cis_o_a, d_o_a, dis_o_a, e_o_a, f_o_a, fis_o_a, g_o_a, gis_o_a = ["A", "AIS", "H", "C","CIS", "D", "DIS", "E", "F", "FIS", "G", "GIS"]
-    d_o_d, dis_o_d, e_o_d, f_o_d, fis_o_d, g_o_d, gis_o_d, a_o_d, ais_o_d, h_o_d, c_o_d, cis_o_d = ["D", "DIS", "E", "F","FIS", "G", "GIS", "A","AIS", "B", "C", "CIS"]
-    g_o_g, gis_o_g, a_o_g, ais_o_g, h_o_g, c_o_g, cis_o_g, d_o_g, dis_o_g, e_o_g, f_o_g, fis_o_g = ["G", "GIS", "A", "AIS", "H", "C","CIS", "D", "DIS", "E", "F", "FIS"]
-    h_o_h, c_o_h, cis_o_h, d_o_h, dis_o_h, e_o_h, f_o_h, fis_o_h, g_o_h, gis_o_h, a_o_h, ais_o_h = ["H", "C", "CIS", "D","DIS", "E", "F", "FIS","G", "GIS", "A","AIS"]
+    e_o_e, f_o_e, fis_o_e,ges_o_e, g_o_e, gis_o_e,as_o_e, a_o_e, ais_o_e,b_o_e,h_o_e, c_o_e, cis_o_e,des_o_e, d_o_e, dis_o_e,es_o_e = ["E", "F", "FIS","GES","G","GIS","AS", "A", "AIS","B", "H", "C", "CIS","DES", "D", "DIS","ES"]
+    e_o_e1, f_o_e1, fis_o_e1,ges_o_e1, g_o_e1, gis_o_e1,as_o_e1, a_o_e1, ais_o_e1,b_o_e1, h_o_e1, c_o_e1, cis_o_e1,des_o_e1, d_o_e1, dis_o_e1,es_o_e1 = ["E", "F","FIS","GES", "G","GIS","AS", "A","AIS","B", "H","C", "CIS","DES","D", "DIS","ES"]
+    a_o_a, ais_o_a,b_o_a, h_o_a, c_o_a, cis_o_a,des_o_a, d_o_a, dis_o_a,es_o_a, e_o_a, f_o_a, fis_o_a,ges_o_a, g_o_a, gis_o_a,as_o_a = ["A", "AIS","B", "H", "C","CIS","DES", "D", "DIS","ES", "E", "F", "FIS","GES", "G", "GIS","AS"]
+    d_o_d, dis_o_d,es_o_d, e_o_d, f_o_d, fis_o_d,ges_o_d, g_o_d, gis_o_d,as_o_d, a_o_d, ais_o_d,b_o_d, h_o_d, c_o_d, cis_o_d,des_o_d = ["D", "DIS","ES", "E", "F","FIS","GES", "G", "GIS","AS", "A","AIS","B", "H", "C", "CIS","DES"]
+    g_o_g, gis_o_g,as_o_g, a_o_g, ais_o_g,b_o_g, h_o_g, c_o_g, cis_o_g,des_o_g, d_o_g, dis_o_g,es_o_g, e_o_g, f_o_g, fis_o_g,ges_o_g = ["G", "GIS","AS", "A", "AIS","B", "H", "C","CIS","DES", "D", "DIS","ES", "E", "F", "FIS","GES"]
+    h_o_h, c_o_h, cis_o_h,des_o_h, d_o_h, dis_o_h,es_o_h, e_o_h, f_o_h, fis_o_h,ges_o_h, g_o_h, gis_o_h,as_o_h, a_o_h, ais_o_h,b_o_h = ["H", "C", "CIS","DES", "D","DIS","ES", "E", "F", "FIS","GES","G", "GIS","AS", "A","AIS","B"]
 
 
     class String:
@@ -231,12 +231,12 @@ def main():
     # string 6 have index 0 string 5 have index 1...
     # For evry number in progi i have to specify a number that describe a sound on a guitar string
 
-    string_E = String(e_o_e, f_o_e, fis_o_e, g_o_e, gis_o_e, a_o_e, ais_o_e, h_o_e, c_o_e, cis_o_e, d_o_e, dis_o_e)
-    string_A = String(e_o_a,f_o_a,fis_o_a,g_o_a,gis_o_a,a_o_a,ais_o_a,h_o_a,c_o_a,cis_o_a,d_o_a,dis_o_a)
-    string_D = String(e_o_d,f_o_d,fis_o_d,g_o_d,gis_o_d,ais_o_a,ais_o_d,h_o_d,c_o_d,cis_o_d,d_o_d,dis_o_d)
-    string_G = String(e_o_g,f_o_g,fis_o_g,g_o_g,gis_o_g,a_o_g,ais_o_g,h_o_g,c_o_g,cis_o_g,d_o_g,dis_o_g)
-    string_H = String(e_o_h,f_o_h,fis_o_h,g_o_h,gis_o_h,a_o_h,ais_o_h,h_o_h,c_o_h,cis_o_h,d_o_h,dis_o_h)
-    string_E1 = String(e_o_e1,f_o_e1,fis_o_e1,g_o_e1,gis_o_e1,a_o_e1,ais_o_e1,h_o_e1,c_o_e1,cis_o_e1,d_o_e1,dis_o_e1)
+    string_E = String(e_o_e, f_o_e,(fis_o_e,ges_o_e), g_o_e, (gis_o_e,as_o_e), a_o_e,(ais_o_e,b_o_e), h_o_e, c_o_e, (cis_o_e,des_o_e), d_o_e, (dis_o_e,es_o_e))
+    string_A = String(e_o_a,f_o_a,(fis_o_a,ges_o_a),g_o_a,(gis_o_a,as_o_a),a_o_a,(ais_o_a,b_o_a),h_o_a,c_o_a,(cis_o_a,des_o_a),d_o_a,(dis_o_a,es_o_a))
+    string_D = String(e_o_d,f_o_d,(fis_o_d,ges_o_d),g_o_d,(gis_o_d,as_o_d),a_o_d,(ais_o_d,b_o_d),h_o_d,c_o_d,(cis_o_d,des_o_d),d_o_d,(dis_o_d,es_o_d))
+    string_G = String(e_o_g,f_o_g,(fis_o_g,ges_o_g),g_o_g,(gis_o_g,as_o_g),a_o_g,(ais_o_g,b_o_g),h_o_g,c_o_g,(cis_o_g,des_o_g),d_o_g,(dis_o_g,es_o_g))
+    string_H = String(e_o_h,f_o_h,(fis_o_h,ges_o_h),g_o_h,(gis_o_h,as_o_h),a_o_h,(ais_o_h,b_o_h),h_o_h,c_o_h,(cis_o_h,des_o_h),d_o_h,(dis_o_h,es_o_h))
+    string_E1 = String(e_o_e1,f_o_e1,(fis_o_e1,ges_o_e1),g_o_e1,(gis_o_e1,as_o_e1),a_o_e1,(ais_o_e1,b_o_e1),h_o_e1,c_o_e1,(cis_o_e1,des_o_e1),d_o_e1,(dis_o_e1,es_o_e1))
 
 
     varH = string_H.strunga_H()
@@ -247,6 +247,7 @@ def main():
         for key in list_of_string_h_keys:
             return key
 
+    exceptionH = list(key_of_h())
 
     varG = string_G.strunga_G()
     stringG_keys = varG.keys()
@@ -257,6 +258,8 @@ def main():
         for key in list_of_string_g_keys:
             return key
 
+    exceptionG = list(key_of_G())
+
 
     varD = string_D.struna_D()
     stringD_keys = varD.keys()
@@ -265,6 +268,8 @@ def main():
     def key_of_d():
         for key in list_of_string_d_keys:
             return key
+
+    exceptionD = list(key_of_d())
 
 
     varA = string_A.struna_A()
@@ -275,80 +280,146 @@ def main():
         for key in list_of_string_a_keys:
             return key
 
+    exceptionA = list(key_of_string_A())
+
+
 
     var = string_E.struna_E()
     stringE_keys = var.keys()
     list_of_string_e_keys = list(stringE_keys)
 
 
+
     #HERE IS THE KEY FOR STRING E
     def key_of_stringE():
-        for key in list_of_string_e_keys:
-            return key
+            for key in list_of_string_e_keys:
+                return key
 
-
-    def outputt():
-        if guess.upper() == odp:
-            print("Correct")
-        else:
-            print("Wrong key")
-
-
+    exceptionE = list(key_of_stringE())
 
     def finall_test():
-        print("Próg " + str(progi) + " struna " + str(random2))
+        print("Fret " + str(progi) + " string " + str(random2))
         guess = input("Enter the sound,or type [quit], to exit: ")
         if random2 == 6 and guess != "quit":
-            odp = key_of_stringE()
-            if guess.upper() == odp:
-                print("Correct")
-                main()
-            else:
-                print("Wrong key")
-                main()
+            if len(key_of_stringE()) != 2:
+                odp = key_of_stringE()
+                if guess.upper() == odp:
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + key_of_stringE())
+                    main()
+            elif len(key_of_stringE()) == 2:
+                odp = exceptionE
+                if guess.upper() in odp and odp != "" and odp != " ":
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + str(exceptionE))
+                    main()
         elif random2 == 5 and guess != "quit":
-            odp = key_of_string_A()
-            if guess.upper() == odp:
-                print("Correct")
-                main()
-            else:
-                print("Wrong key")
-                main()
+            if len(key_of_string_A()) != 2:
+                odp = key_of_string_A()
+                if guess.upper() == odp:
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + key_of_string_A())
+                    main()
+            elif len(key_of_string_A()) == 2:
+                odp = exceptionA
+                if guess.upper() in odp and odp != "" and odp != " " and odp != "  ":
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + str(exceptionA))
+                    main()
         elif random2 == 4 and guess != "quit":
-            odp = key_of_d()
-            if guess.upper() == odp:
-                print("Correct")
-                main()
-            else:
-                print("Wrong key")
-                main()
+            if len(key_of_d()) != 2:
+                odp = key_of_d()
+                if guess.upper() == odp:
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + key_of_d())
+                    main()
+            elif len(key_of_d()) == 2:
+                odp = exceptionD
+                if guess.upper() in odp and odp != "" and odp != " " and odp != "  ":
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + str(exceptionD))
+                    main()
         elif random2 == 3 and guess != "quit":
-            odp = key_of_G()
-            if guess.upper() == odp:
-                print("Correct")
-                main()
-            else:
-                print("Wrong key")
-                main()
+            if len(key_of_G()) != 2:
+                odp = key_of_G()
+                if guess.upper() == odp:
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + key_of_G())
+                    main()
+            elif len(key_of_G()) == 2:
+                odp = exceptionG
+                if guess.upper() in odp and odp != "" and odp != " " and odp != "  ":
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + str(exceptionG))
+                    main()
         elif random2 == 2 and guess != "quit":
-            odp = key_of_h()
-            if guess.upper() == odp:
-                print("Correct")
-                main()
-            else:
-                print("Wrong key")
-                main()
+            if len(key_of_h()) != 2:
+                odp = key_of_h()
+                if guess.upper() == odp:
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + key_of_h())
+                    main()
+            elif len(key_of_h()) == 2:
+                odp = exceptionH
+                if guess.upper() in odp and odp != "" and odp != " " and odp != "  ":
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + str(exceptionH))
+                    main()
         elif random2 == 1 and guess != "quit":
-            odp = key_of_stringE()
-            if guess.upper() == odp:
-                print("Correct")
-                main()
-            else:
-                print("Wrong key")
-                main()
-        elif guess == "quit":
-            return quit()
+            if len(key_of_stringE()) != 2:
+                odp = key_of_stringE()
+                if guess.upper() == odp:
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + key_of_stringE())
+                    main()
+            elif len(key_of_stringE()) == 2:
+                odp = exceptionE
+                if guess.upper() in odp and odp != "" and odp != " ":
+                    print("Correct")
+                    main()
+                else:
+                    print("Wrong key")
+                    print("The correct answer is: " + str(exceptionE))
+                    main()
 
     finall_test()
 
+
 main()
+
+
+
+
